0ef24cc3b04f6918dc32f018f83601d3b8484a57
David Rothlisberger <david@rothlis.net>
2012-05-26 09:14:36 +0100

    Don't print implicit declarations like __va_list_tag.
    I don't know exactly where these come from, but I certainly don't want them
    in my tags file.
    
    Their location.file is null ("None" in python-speak), so I can easily filter
    them out.

--- test/tags.sh
+++ test/tags.sh
@@ -3,3 +3,9 @@ test_basic_tags() {
     assert_tag n1 6,120
     assert_tag s 7,139
 }
+
+test_implicit_declarations_arent_tagged() {
+    clang-ctags macros.cpp > TAGS
+    assert_no_tag __int128_t
+    assert_no_tag __va_list_tag
+}
