f8769b380907acda5ca7150677217738594ad7e7
David Rothlisberger <david@rothlis.net>
2012-07-13 17:22:00 +0100

    Process multiple source files (including header files)
    specified on command line, when `--compile-commands` is given with the
    database for looking up each file's compilation command line.
    
    Note that I can now index header files specified on the command line, as
    long as I also specify (on the command line) a cpp file (with an entry
    in the compile commands database) that includes the header file.
    
    Up until this commit I had no way of choosing which header files to
    index; my choices were to generate tags for:
    
    1. all header files (including every header recursively included by
    other headers), or
    2. perhaps only header files in the same directory as the source
    file (or under the directory where clang-ctags was invoked), or
    3. no header files at all.
    
    Number 3 is what I was doing up until this commit, and is still what I
    do when invoked without `--compile-commands`: I only generate tags when
    cursor.location.file.name == tu.spelling.

--- test/compile_commands.sh
+++ test/compile_commands.sh
@@ -2,3 +2,12 @@ test_db() {
     clang-ctags -e --compile-commands=compile_commands.json subdir/a.cpp
     assert_tag a
 }
+
+test_db_with_multiple_source_files() {
+    clang-ctags -e --compile-commands=compile_commands.json \
+        subdir/a.cpp subdir/b.cpp subdir/b.h
+    assert_tag a
+    assert_tag b
+    assert_emacs '(find-tag "a")' a.cpp:1
+    assert_emacs '(find-tag "b")' b.h:1
+}
